7e6873fe49ba602d41e8d09d898a4d6440a9650c
davidtnfsh <davidtnfsh@gmail.com>
2016-10-26 10:52:45 +0800

    turn it from django app into django project, which can be deploy independently

---
+++ PTT_KCM_API/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PTT_KCM_APIConfig(AppConfig):
+    name = 'PTT_KCM_API'
--- PTT_KCM_API/views.py
+++ PTT_KCM_API/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # import api from view directory
-from PTT_factory.api.ip_of_comment import ip
+from PTT_KCM_API.api.ip_of_comment import ip
 
 # Create your views here.
