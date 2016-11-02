98bfc5b5061b9c223d1e56b461d8628bc14eb5a7
davidtnfsh <davidtnfsh@gmail.com>
2016-11-02 17:58:34 +0800

    新增Cross-origin resource sharing設定，否則不同網域的網站無法請求資源 + Ip的db預設值改為空字串 + 更新Requirements.txt

--- PTT_KCM_API/models.py
+++ PTT_KCM_API/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class IP(models.Model):
 	ip = models.CharField(max_length=15)
-	continentName = models.CharField(max_length=15, default='taiwan')
-	countryName = models.CharField(max_length=20, default='taiwan')
-	stateProv = models.CharField(max_length=30, default='taiwan')
-	city = models.CharField(max_length=30, default='taiwan')
+	continentName = models.CharField(max_length=15, default='')
+	countryName = models.CharField(max_length=20, default='')
+	stateProv = models.CharField(max_length=30, default='')
+	city = models.CharField(max_length=30, default='')
 	def __str__(self):
 		return self.ip
 
