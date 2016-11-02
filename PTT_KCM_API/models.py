9520ec3d53a4c06c754e66e6824817b3fb5ef982
davidtnfsh <davidtnfsh@gmail.com>
2016-11-02 15:34:04 +0800

    新增model migrations以及default值

--- PTT_KCM_API/models.py
+++ PTT_KCM_API/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class IP(models.Model):
 	ip = models.CharField(max_length=15)
-	continentName = models.CharField(max_length=15)
-	countryName = models.CharField(max_length=20)
-	stateProv = models.CharField(max_length=30)
-	city = models.CharField(max_length=30)
+	continentName = models.CharField(max_length=15, default='taiwan')
+	countryName = models.CharField(max_length=20, default='taiwan')
+	stateProv = models.CharField(max_length=30, default='taiwan')
+	city = models.CharField(max_length=30, default='taiwan')
 	def __str__(self):
 		return self.ip
 
