cb843e9653d6a44a10d2351451e298c501e9c2b4
davidtnfsh <davidtnfsh@gmail.com>
2017-01-13 15:45:26 +0800

    make insert articles once with an array + and also create index on invertedIndex collection

--- PTT_KCM_API/views.py
+++ PTT_KCM_API/views.py
@@ -12,41 +12,40 @@ def buildArticle2DB(request, uri=None):
 	from pymongo import MongoClient
 	from PTT_KCM_API.view.dictionary.postokenizer import PosTokenizer
 	from PTT_KCM_API.trigger_cache.trigger_cache import trigger_cache
-	import json, pyprind
+	import json, pyprind, pymongo
 
 	client = MongoClient(uri)
 	db = client['ptt']
 	articlesCollect = db['articles']
 	IndexCollect = db['invertedIndex']
+	articlesCollect.remove({})
 	IndexCollect.remove({})
 	db['ip'].remove({})
 	db['locations'].remove({})
 
 	key = dict()
+	articleList = []
 	p = pttJson()
 
 	f = json.load(open(p.filePath, 'r', encoding='utf8'))
-	for i in pyprind.prog_percent(f['articles']):
 
+	articlesCollect.insert(f['articles'])
+
+	for i in articlesCollect.find():
 		if i.get('article_id', None) == None:
 			continue
 
-		cursor = articlesCollect.find({'article_id':i['article_id']}).limit(1)
-		if cursor.count() ==0:
-			print('insert')
-			article = articlesCollect.update({'article_id':i['article_id']}, i, upsert = True)
-			objectID = article['upserted']
-		else:
-			print('exists')
-			objectID = dict(list(cursor)[0])['_id']
-
+		objectID = i['_id']
+		
 		uniqueTerm = set(PosTokenizer('' if i.get('article_title', '')==None else i.get('article_title', ''), ['n']))
 		uniqueTerm = uniqueTerm.union(PosTokenizer('' if i.get('content', '')==None else i.get('content', ''), ['n']))
 		for k in uniqueTerm:
 			key.setdefault(k, []).append(objectID)
 
-	for k, v in key.items():
-		IndexCollect.update({'issue':k}, {'ObjectID':v, 'issue':k}, upsert=True)
+
+	IndexList = tuple({'ObjectID':v, 'issue':k} for k, v in key.items())
+	IndexCollect.insert(IndexList)
+	IndexCollect.create_index([("issue", pymongo.HASHED)])
 
 	trigger_cache()
 	return JsonResponse({"status":"success"})
