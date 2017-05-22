ad285fa4d88a153419a456a440a53a3b79be656d
david <davidtnfsh@gmail.com>
2017-05-22 11:24:18 +0800

    add swinger to requirements.txt + modified management let insertArticles can use append flag

--- PTT_KCM_API/management/commands/insertArticles.py
+++ PTT_KCM_API/management/commands/insertArticles.py
@@ -6,51 +6,58 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         # Positional arguments
         parser.add_argument('json', type=str)
+        parser.add_argument(
+            '--append',
+            default=False,
+            help='An optional argument, if Append==True, then it wont empty mongoDB and append new articles in it. Otherwise, empty MongoDB before inserting articles.',
+        )
+
 
     def handle(self, *args, **options):
-    	from project.settings_database import uri
-    	from pymongo import MongoClient
-    	from PTT_KCM_API.view.dictionary.postokenizer import PosTokenizer
-    	import json, pyprind, pymongo
-    	client = MongoClient(uri)
-    	db = client['ptt']
-    	articlesCollect = db['articles']
-    	IndexCollect = db['invertedIndex']
-    	articlesCollect.remove({})
-    	IndexCollect.remove({})
-    	db['ip'].remove({})
-    	db['locations'].remove({})
+        from project.settings_database import uri
+        from pymongo import MongoClient
+        from PTT_KCM_API.view.dictionary.postokenizer import PosTokenizer
+        import json, pyprind, pymongo
+        client = MongoClient(uri)
+        db = client['ptt']
+        articlesCollect = db['articles']
+        IndexCollect = db['invertedIndex']
+        if options['append']==False:
+            articlesCollect.remove({})
+            IndexCollect.remove({})
+            db['ip'].remove({})
+            db['locations'].remove({})
 
-    	key = dict()
-    	articleList = []
+        key = dict()
+        articleList = []
 
-    	f = json.load(open(options['json'], 'r', encoding='utf-8-sig'))
+        f = json.load(open(options['json'], 'r', encoding='utf-8-sig'))
 
-    	articlesCollect.insert(f['articles'])
+        articlesCollect.insert(f['articles'])
 
-    	bar = pyprind.ProgBar( articlesCollect.find().count())
-    	for i in articlesCollect.find().batch_size(500):
-    		# pymongo Cursor with timeout if time of query data exceed 10 minutes.
-    		# so setting batch_size will fetch amount of document from mongo 
-    		# in per query.
-    		# But there is no universal "right" batch_size
-    		# You should test with different values and see what is the appropriate value for your use case i.e. how many documents can you process in a 10 minute window.
-    		# http://stackoverflow.com/questions/24199729/pymongo-errors-cursornotfound-cursor-id-not-valid-at-server
+        bar = pyprind.ProgBar( articlesCollect.find().count())
+        for i in articlesCollect.find().batch_size(500):
+            # pymongo Cursor with timeout if time of query data exceed 10 minutes.
+            # so setting batch_size will fetch amount of document from mongo 
+            # in per query.
+            # But there is no universal "right" batch_size
+            # You should test with different values and see what is the appropriate value for your use case i.e. how many documents can you process in a 10 minute window.
+            # http://stackoverflow.com/questions/24199729/pymongo-errors-cursornotfound-cursor-id-not-valid-at-server
 
-    		bar.update()
-    		if i.get('article_id', None) == None:
-    			continue
+            bar.update()
+            if i.get('article_id', None) == None:
+                continue
 
-    		objectID = i['_id']
-    		
-    		uniqueTerm = set(PosTokenizer('' if i.get('article_title', '')==None else i.get('article_title', ''), save=['n', 'l', 'eng']))
-    		uniqueTerm = uniqueTerm.union(PosTokenizer('' if i.get('content', '')==None else i.get('content', ''), save=['n', 'l', 'eng']))
-    		for k in uniqueTerm:
-    			key.setdefault(k, []).append(objectID)
+            objectID = i['_id']
+            
+            uniqueTerm = set(PosTokenizer('' if i.get('article_title', '')==None else i.get('article_title', ''), save=['n', 'l', 'eng']))
+            uniqueTerm = uniqueTerm.union(PosTokenizer('' if i.get('content', '')==None else i.get('content', ''), save=['n', 'l', 'eng']))
+            for k in uniqueTerm:
+                key.setdefault(k, []).append(objectID)
 
 
-    	IndexList = tuple({'ObjectID':v, 'issue':k} for k, v in key.items())
-    	IndexCollect.insert(IndexList)
-    	IndexCollect.create_index([("issue", pymongo.HASHED)])
+        IndexList = tuple({'ObjectID':v, 'issue':k} for k, v in key.items())
+        IndexCollect.insert(IndexList)
+        IndexCollect.create_index([("issue", pymongo.HASHED)])
 
-    	self.stdout.write(self.style.SUCCESS('insert Articles success!!!'))
+        self.stdout.write(self.style.SUCCESS('insert Articles success!!!'))
